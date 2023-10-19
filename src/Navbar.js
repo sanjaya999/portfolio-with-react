6e20656312175bcafbe247313901afd0d445a0b6
sanjaya poudel <poudelsanjaya12@gmail.com>
2023-10-19 20:20:01 +0545

    my portfolio website 1st day

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+function Nav(){
+    return(
+        <div class="nav">
+          <ul>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#"> Contact</a> </li>
+            <li><a href="#"> Project</a></li>
+          </ul>
+        </div>
+    )
+}
+
+export default Nav;
